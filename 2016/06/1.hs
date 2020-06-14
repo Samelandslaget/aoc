75312ada996d3221d5fdee2eb2b0655bf4ebb863
Vidar Bratlie <vidar.bratlie@bcc.no>
2020-06-14 23:43:19 +0200

    Complete 2016 day 6 part 1

---
+++ 2016/06/1.hs
@@ -0,0 +1,26 @@
+{-
+-}
+
+import System.Environment
+import Data.List
+
+main = do
+    args <- getArgs
+    content <- readFile (head args)
+    let ls = lines content
+    let columns = transpose' ls
+    putStrLn (map mostCommon columns)
+
+transpose' :: [String] -> [String]
+transpose' ls = [ [ l!!i | l <- ls ] | i <- [0..length (head ls)-1] ]
+
+mostCommon :: String -> Char
+mostCommon s = head (head [x | x <- grouped, (length x) == maxLength])
+    where maxLength = last $ sort $ map length grouped
+          grouped = groupBy' s
+
+groupBy' :: String -> [String]
+groupBy' "" = []
+groupBy' str
+    | str /= (sort str) = groupBy' (sort str)
+groupBy' (x:xs) = takeWhile (\a -> a == x) (x:xs) : (groupBy' (dropWhile (\a -> a == x) (x:xs)))
