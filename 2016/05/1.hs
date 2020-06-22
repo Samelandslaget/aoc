002f266d9cf71e3232ee082ce36ce5c26f40fc3a
Vidar Bratlie <vidar.bratlie@bcc.no>
2020-06-22 20:59:07 +0200

    Complete 2016 day 5 part 1

--- 2016/05/1.hs
+++ 2016/05/1.hs
@@ -3,15 +3,14 @@ import qualified Crypto.Hash.MD5 as CHM
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Base16 as BSB
 
+-- NOTE: The actual input should be the argument to main, not the file name of the text file containing the input.
 main = do
     args <- getArgs
-    doorId <- readFile (head args)
-    putStr doorId
+    let doorId = head args
+    let pwd = generatePwd doorId
+    putStr pwd
+
+generatePwd :: String -> String
+generatePwd doorId = map (!!5) $ take 8 $ filter (\x -> (take 5 x) == "00000" ) $ map md5 $ map (doorId++) $ map show [0..]
 
 md5 str = BSC.unpack . BSB.encode . CHM.hash . BSC.pack $ str
-
--- WARNING: Infinite!
--- infPass = [x | x <- ()]
-
-
--- Note: this file is a WIP
